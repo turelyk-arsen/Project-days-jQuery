19d2c7c7213371a6936379ad395338e0d65f6423
turelyk-arsen <114808096+turelyk-arsen@users.noreply.github.com>
2023-03-04 23:32:11 +0100

    04/03/2023

---
+++ scripts/arrays.js
@@ -0,0 +1,71 @@
+const goalsArray = [
+    {
+      name: "Maserati Guibli",
+      picture: "images/goals/maserati.png",
+      goal: 85000,
+      current: 68000,
+      riched: false,
+    },
+    {
+      name: "Playstation 4",
+      picture: "images/goals/ps4.png",
+      goal: 800,
+      current: 150,
+      riched: false,
+    },
+    {
+      name: "Samsung Galaxy S10e",
+      picture: "",
+      goal: 1000,
+      current: 300,
+      riched: false,
+    },
+    {
+      name: "Boat",
+      picture: "",
+      goal: 10000,
+      current: 10000,
+      riched: true,
+    },
+    {
+      name: "Rolex",
+      picture: "",
+      goal: 5000,
+      current: 5000,
+      riched: true,
+    },
+  ];
+
+const messagesArray = [
+    {
+        sender :'Thomas Johnson',
+        lastMessage :'Hello, man. Give me $120, please. Cat gives birth...' ,
+        date : '5 min ago',
+        notSeen :1,
+        picture : '/images/contacts/contact1.png'
+    },
+    {
+        sender :'Steve Bruski',
+        lastMessage :'Thanks. I really helped out. I\'ll give it the day after tom...' ,
+        date : 'Today, at 10:00',
+        notSeen :1,
+        picture : '/images/contacts/contact2.png'
+    },
+    {
+        sender :'Anastasia Markus',
+        lastMessage :'Return at least once in time 😈' ,
+        date : 'Yesterday, at 14:02',
+        picture : '/images/contacts/contact3.png'
+    },
+    {
+        sender :'Anastasia Markus',
+        lastMessage :'No, don’t ask me for a loan anymore. The freebie is over ✋' ,
+        date : 'Yesterday, at 13:45',
+        picture : '/images/contacts/contact4.png'
+    },
+    {
+        sender :'e Banco',
+        lastMessage :'Dear users, today at 00:00 the service will be unavaila...' ,
+        date : '20 Dec, at 09:00',
+    },
+]
--- scripts/script.js
+++ scripts/script.js
@@ -1,47 +1,9 @@
-const goalsArray = [
-  {
-    name: "Maserati Guibli",
-    picture: "images/goals/maserati.png",
-    goal: 85000,
-    current: 68000,
-    riched: false,
-  },
-  {
-    name: "Playstation 4",
-    picture: "images/goals/ps4.png",
-    goal: 800,
-    current: 150,
-    riched: false,
-  },
-  {
-    name: "Samsung Galaxy S10e",
-    picture: "",
-    goal: 1000,
-    current: 300,
-    riched: false,
-  },
-  {
-    name: "Boat",
-    picture: "",
-    goal: 10000,
-    current: 10000,
-    riched: true,
-  },
-  {
-    name: "Rolex",
-    picture: "",
-    goal: 5000,
-    current: 5000,
-    riched: true,
-  },
-];
-
-const model = $("article");
+const model = $("#goals article");
 for (const userData of goalsArray) {
   // 1 - create / clone
   const clone = model.clone();
   // 2 - append
-  $("section").append(clone);
+  $("#goals").append(clone);
   // 3 - customize
   clone.find("h2").text(userData.name);
   clone.find("h4").text("$" + userData.goal);
@@ -75,9 +37,9 @@ for (let i = 0; i < goalsArray.length; i++) {
   if (goalsArray[i].riched == false) {
     numFalse++;
     numTrue = goalsArray.length - numFalse;
-    $("article").eq(i).addClass([goalsArray[i].riched]);
+    $("#goals article").eq(i).addClass([goalsArray[i].riched]);
   } else {
-    $("article").eq(i).addClass([goalsArray[i].riched]);
+    $("#goals article").eq(i).addClass([goalsArray[i].riched]);
   }
 }
 
@@ -108,3 +70,37 @@ $("#reached").on("click", function () {
   $(".true").slideDown("2000");
   $(".false").slideUp("2000");
 });
+
+const modelMessages = $("#messages article");
+for (const messagesData of messagesArray) {
+  const cloneMessages = modelMessages.clone();
+  $("#messages").append(cloneMessages);
+
+  cloneMessages.find("h2").text(messagesData.sender);
+  cloneMessages.find("p").text(messagesData.lastMessage);
+  cloneMessages.find("span").text(messagesData.date);
+
+  if (messagesData.notSeen) {
+    cloneMessages.find("#notSeen").addClass('notSeen');
+    cloneMessages.find(".notSeen").text(messagesData.notSeen);
+  }
+
+  if (messagesData.picture) {
+      cloneMessages.find("img").attr("src", messagesData.picture);
+  } else {
+    cloneMessages.find("img").remove();
+    // cloneMessages.find("article>div").html('<p>' + messagesData.sender.slice(0,3)+'</p>');
+    cloneMessages.find('#imageNot').text(messagesData.sender.slice(0,3));
+    cloneMessages.find('#imageNot').addClass('imageNot');
+
+          console.log(messagesData.sender.slice(0,3))
+
+  }
+
+  // clone.css("background-image", "url(" + userData.picture + ")");
+
+  // let length = (userData.current / userData.goal) * 100;
+  // clone.find("#myBar").width(length + "%");
+}
+// kill the model
+modelMessages.remove();
